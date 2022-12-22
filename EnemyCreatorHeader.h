c86c62347c3477fed215bc94daba0a7dfec9f28c
Iznhou <90870636+Iznhou@users.noreply.github.com>
2022-12-22 18:32:29 -0500

    Update EnemyCreatorHeader.h

--- EnemyCreatorHeader.h
+++ EnemyCreatorHeader.h
@@ -1,25 +1,29 @@
 #pragma once
 
+
 #include<string>
 
 using namespace std;
 
 class EnemyCreator
 {
+	//Private variables
 	private:
 		string enemyName;
 		string weapon;
 		string characterClass;
 		int level;
 
-public:
-	EnemyCreator(string name, int lvl, int enemyID);
-	string setEnemyName(string name);
-	string setWeapon(string weapon);
-	/*string setCharacterClass(string cClass);
-	int setCharacterLevel(int lvl);*/
+	//Public functions and constructor
+	public:
+		EnemyCreator(string name, int lvl, int enemyID);
+		string setEnemyName(string name);
+		string setWeapon(string weapon);
+		/*string setCharacterClass(string cClass);
+		int setCharacterLevel(int lvl);*/
 
-	void enemyGenerate();
+		void enemyGenerate();
 
-	~EnemyCreator();
+		~EnemyCreator();
 };
+
